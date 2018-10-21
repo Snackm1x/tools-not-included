81a0522c6910befd55e75ca3d64d804020533b49
Cairath <Cairath@users.noreply.github.com>
2018-10-21 23:55:14 +0200

    Unregister service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as WebFont from 'webfontloader';
 
-
 import './index.css';
 import Root from './pages/Root';
 import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 var ReactGA = require('react-ga');
 
@@ -20,3 +20,4 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<Root />, document.getElementById('root') as HTMLElement);
 //registerServiceWorker();
+unregister();
