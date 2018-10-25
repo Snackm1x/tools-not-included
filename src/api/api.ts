a04f9947fe88143d08a0dfbda66518fb794dc4ea
Cairath <Cairath@users.noreply.github.com>
2018-10-25 16:47:02 +0200

    Model for browser filter

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: process.env.NODE_ENV === "production" ? 'https://oni-seed-browser-api.herokuapp.com/api/' : 'http://localhost:50164/api/'
+    //baseURL: process.env.NODE_ENV === "production" ? 'https://oni-seed-browser-api.herokuapp.com/api/' : 'http://localhost:50164/api/'
+    baseURL: 'https://oni-seed-browser-api.herokuapp.com/api/'
 });
---
+++ src/api/dto/SeedListFIlterDTO.ts
@@ -0,0 +1,15 @@
+export default interface SeedListFilterDTO {
+    rowsPerPage: number,
+    page: number,
+
+    seed: number;
+    gameUpgrades: string[];
+    earliestVersionNumber: number;
+    latestVersionNumber: number;
+    geysers: {
+        type: string;
+        min?: number;
+    }[];
+
+    modOnly: boolean;
+}
