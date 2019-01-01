a96b1d21a0f4f7d6c04b15030c3f0505fd9cf3d5
Cairath <Cairath@users.noreply.github.com>
2019-01-01 04:17:57 +0100

    404 page

--- src/pages/coffee/CoffeePage.tsx
+++ src/pages/coffee/CoffeePage.tsx
@@ -13,7 +13,7 @@ class CoffeePage extends React.Component<AllProps> {
 			<div style={{ display: 'flex', minHeight: '70%', alignItems: 'center' }}>
 				<div>
 					<Row type='flex' gutter={16} style={{ marginTop: 24 }}>
-						<Col xs={24} md={12} offset={2} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center' }}>
+						<Col xs={24} md={12} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center', marginLeft: 'auto', paddingLeft: 24, paddingRight: 24 }}>
 							<h1>{t('coffee.header')}</h1>
 							<p style={{ textAlign: 'center' }}>{t('coffee.text-1')}</p>
 							<p style={{ textAlign: 'center' }}>{t('coffee.text-2')}</p>
@@ -21,7 +21,7 @@ class CoffeePage extends React.Component<AllProps> {
 							<KofiButton />
 						</Col>
 						<Col xs={24} md={10} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-							<img id="coffee" src='/images/coffee.png' style={{ maxHeight: 240, objectFit: 'contain' }} />
+							<img id="coffee" src='/images/coffee.png' style={{ maxHeight: 240, objectFit: 'contain', margin: 24 }} />
 						</Col>
 					</Row>
 				</div>
--- src/pages/error/Error404.tsx
+++ src/pages/error/Error404.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react';
+import { Col, Row, Button } from 'antd';
+import { withNamespaces, WithNamespaces } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
-import { Layout } from 'antd';
+class Error404 extends React.Component<WithNamespaces> {
 
-
-
-class Error404 extends React.Component {
 	public render() {
+		const { t } = this.props;
 		return (
-			<div>404</div>
+			<div style={{ display: 'flex', minHeight: '70%', alignItems: 'center' }}>
+				<Row type='flex' gutter={16} style={{ marginTop: 24, marginBottom: 24, width: '100%' }}>
+					<Col xs={24} md={10} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+						<img id="outhouse" src='/images/outhouse.png' style={{ maxHeight: 500, objectFit: 'contain' }} />
+					</Col>
+					<Col xs={24} md={12} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+						<h1>Crap, nothing to see here!</h1>
+						<br />
+						<p style={{ textAlign: 'center' }}>
+							Go back to the main page.
+							<br />
+							If you believe you're seeing this incorrectly, please contact the developer.
+						</p>
+						<br /><br />
+						<Link to="/"><Button type="primary" >Return to the Main Page</Button></Link>
+					</Col>
+				</Row>
+			</div>
 		);
 	}
 }
 
-export default Error404;
+export default withNamespaces()(Error404);
