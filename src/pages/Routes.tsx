f951ecf81156ba391b1adc4f31084613473a5ffc
Cairath <Cairath@users.noreply.github.com>
2019-01-01 22:54:30 +0100

    Error 500 page

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -5,6 +5,7 @@ import CoffeePage from './coffee/CoffeePage';
 import Error404 from './error/Error404';
 import SeedDetailsPage from './seeds/details/SeedDetailsPage';
 import SeedModInfo from './seeds/mod-info/SeedModInfo';
+import Error500 from './error/Error500';
 
 const Routes = () => (
     <Switch>
@@ -13,6 +14,8 @@ const Routes = () => (
         <Route exact path="/seeds/modinfo" component={SeedModInfo} />
         <Route exact path="/seeds" component={SeedBrowserPage} />
         <Route exact path="/coffee" component={CoffeePage} />
+        <Route exact path="/500" component={Error500} />
+        <Route exact path="/404" component={Error404} />
         <Route component={Error404}/>
     </Switch>
 );
--- src/pages/error/Error500.tsx
+++ src/pages/error/Error500.tsx
@@ -1,15 +1,33 @@
 import * as React from 'react';
+import { Col, Row, Button } from 'antd';
+import { withNamespaces, WithNamespaces } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
-import { Layout } from 'antd';
+class Error500 extends React.Component<WithNamespaces> {
 
-
-
-class Error500 extends React.Component {
 	public render() {
+		const { t } = this.props;
 		return (
-			<div>500</div>
+			<div style={{ display: 'flex', minHeight: '70%', alignItems: 'center' }}>
+				<Row type='flex' gutter={16} style={{ marginTop: 24, marginBottom: 24, width: '100%' }}>
+					<Col xs={24} md={10} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+						<img id="ohno" src='/images/ohno.png' style={{ maxHeight: 500, objectFit: 'contain' }} />
+					</Col>
+					<Col xs={24} md={12} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+						<h1>Oh no! Something went wrong on the server.</h1>
+						<br />
+						<p style={{ textAlign: 'center' }}>
+							Duct tape is being used to patch the holes - sorry for the inconvenience!
+							<br />
+							If the error keeps occuring, please contact the developer.
+						</p>
+						<br /><br />
+						<Link to="/"><Button type="primary" >Return to the Main Page</Button></Link>
+					</Col>
+				</Row>
+			</div>
 		);
 	}
 }
 
-export default Error500;
+export default withNamespaces()(Error500);
