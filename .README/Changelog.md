7115832bc4968e9129ef453c6b6cf2c8adf70495
Cairath <Cairath@users.noreply.github.com>
2019-01-12 01:13:54 +0100

    Changelog loaded from github

---
+++ .README/Changelog.md
@@ -0,0 +1,31 @@
+# Changelog
+### 8 January 2019
+* Hotfix for mods addressing issue that was preventing connection.
+
+### 7 January 2019
+Out of beta! Yay! Due to world element composition being collected, worlds uploaded need to be fresh - within 60 seconds of creation and with no cells dug. If your seed turns out to be great mid-game, you can still create a fresh world with that seed and upload it!
+* The entire front-end rewritten
+* The seed browser filter has been expanded with a custom rule builder
+* Seed details page shows a star map
+* Various performance improvements
+
+From now on, two mods will be maintained: manual and automatic. Automatic version (aka the seed miner - not suitable for playing, seed uploading only) works in background and automatically generates and uploads new worlds without interaction. For more information please refer to the [Mod Info Page](https://toolsnotincluded.net/seeds/modinfo)
+* Mod versions 1.0a(utomatic) and 1.0m(anual):
+  * Mods do not require debug mode anymore
+  * Collecting data about world element composition
+* Mod version 1.0m:
+  * Constraint: uploaded worlds need to be within 60 seconds of their creation and with no cells dug out.
+
+### December 2018
+* Updated roughly 1000 seeds so that there are no more seeds left without geyser detailed statistcs or lack of Oil Reservoir info
+
+### 23 November 2018
+* Mod version 0.3.0 and 0.4.0 (hotfix):
+  * Collection of planet and geyser location data
+
+### 21 October 2018
+* Mod version 0.2.0:
+  * Collecting Oil Reservoir data
+
+### 19 October 2018
+Initial demo release
--- src/pages/changelog/ChangelogPage.tsx
+++ src/pages/changelog/ChangelogPage.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
-import { Spin } from 'antd';
+import { Spin, Alert } from 'antd';
 import * as Markdown from 'react-markdown';
-import './changelog.less'
+import './changelog.less';
+import Axios from 'axios';
 
 interface Props {}
 
 interface State {
 	loading: boolean;
+	changelogText: string | undefined;
+	changelogError: boolean;
 }
 
 class ChangelogPage extends React.Component<Props, State> {
@@ -14,8 +17,16 @@ class ChangelogPage extends React.Component<Props, State> {
 		super(props);
 
 		this.state = {
-			loading: true
+			loading: true,
+			changelogText: undefined,
+			changelogError: false
 		};
+	}
+
+	componentWillMount() {
+		Axios.get<string>('https://raw.githubusercontent.com/Cairath/tools-not-included/master/.README/Changelog.md')
+			.then((res) => this.setState({ changelogText: res.data }))
+			.catch(() => this.setState({ changelogError: true }));
 	}
 
 	componentDidMount() {
@@ -26,39 +37,14 @@ class ChangelogPage extends React.Component<Props, State> {
 		return (
 			<Spin spinning={this.state.loading} wrapperClassName="nontransparent fixed" size="large">
 				<div id="changelog">
-                    <Markdown source="
-# Changelog
-### 8 January 2019
-* Hotfix for mods addressing issue that was preventing connection.
-
-### 7 January 2019
-Out of beta! Yay! Due to world element composition being collected, worlds uploaded need to be fresh - within 60 seconds of creation and with no cells dug. If your seed turns out to be great mid-game, you can still create a fresh world with that seed and upload it!
-* The entire front-end rewritten
-* The seed browser filter has been expanded with a custom rule builder
-* Seed details page shows a star map
-* Various performance improvements
-
-From now on, two mods will be maintained: manual and automatic. Automatic version (aka the seed miner - not suitable for playing, seed uploading only) works in background and automatically generates and uploads new worlds without interaction. For more information please refer to the [Mod Info Page](/seeds/modinfo)
-* Mod versions 1.0a(utomatic) and 1.0m(anual):
-  * Mods do not require debug mode anymore
-  * Collecting data about world element composition
-* Mod version 1.0m:
-  * Constraint: uploaded worlds need to be within 60 seconds of their creation and with no cells dug out.
-
-### December 2018
-* Updated roughly 1000 seeds so that there are no more seeds left without geyser detailed statistcs or lack of Oil Reservoir info
-
-### 23 November 2018
-* Mod version 0.3.0 and 0.4.0 (hotfix):
-  * Collection of planet and geyser location data
-
-### 21 October 2018
-* Mod version 0.2.0:
-  * Collecting Oil Reservoir data
-
-### 19 October 2018
-Initial demo release
-					"/>
+					<Alert
+						message="Can't load changelog"
+						description="Sorry! An error has occured, but the changelog is also available on Github."
+						type="error"
+						showIcon
+						style={{ visibility: this.state.changelogError ? 'visible' : 'collapse' }}
+					/>
+					<Markdown source={this.state.changelogText} />
 				</div>
 			</Spin>
 		);
